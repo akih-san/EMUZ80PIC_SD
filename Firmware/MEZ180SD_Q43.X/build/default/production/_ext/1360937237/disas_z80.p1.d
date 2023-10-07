d1fc49cb6abfd12c14327885b46f297c268a7845
@Akih_san <112370246+akih-san@users.noreply.github.com>
2023-10-07 16:29:11 +0900

    Add files via upload

---
+++ Firmware/MEZ180SD_Q43.X/build/default/production/_ext/1360937237/disas_z80.p1.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1360937237/disas_z80.p1:  \
+../src/disas_z80.c  \
+../src/../src/disas_z80.h  \
+../src/../src/disas.h 
---
+++ Firmware/MEZ180SD_Q43.X/build/default/production/_ext/1360937237/monitor.p1.d
@@ -0,0 +1,16 @@
+build/default/production/_ext/1360937237/monitor.p1:  \
+../src/monitor.c  \
+../src/../src/supermez80.h  \
+../src/../src/picconfig.h  \
+../src/../fatfs/ff.h  \
+../src/../fatfs/../fatfs/ffconf.h  \
+../src/chk_borad_dpend.h  \
+../src/../drivers/mcp23s08.h  \
+../src/../drivers/utils.h  \
+../src/../src/disas.h  \
+../src/../src/disas_z80.h  \
+../src/../drivers/SDCard.h  \
+../src/../drivers/fatdisk_debug.h  \
+../src/../z80/trampoline.inc  \
+../src/../z80/trampoline_cleanup.inc  \
+../src/../z80/trampoline_nmi.inc 
